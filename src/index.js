b587617e0e4b335be16f811688712b5bb0701ebf
Patrick Spek <p.spek@tyil.nl>
2017-04-18 15:54:29 +0200

    Implement auth

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// set basic variables
+const port = process.env.app_port || 3000;
+const controllerDir = "./controller/";
+
+// require dependencies
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// instantiate application
+const app = express();
+
+// add application-wide configuration
+app.set("models", require("./models"));
+
+// apply middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(require("./middlewares/bearer"));
+
+// add controllers
+[
+  "auth",
+  "campaign"
+].forEach(x => app.use("/" + x, require("./controller/" + x)));
+
+// start the application
+app.listen(port, () => console.log("Starting the LikertAPI on port " + port));
+
