67bc4b763674cfa3e26cf6318115f38aa62edcb0
Patrick Spek <p.spek@tyil.nl>
2017-04-19 23:46:55 +0200

    Chain router methods

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -2,103 +2,96 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt-nodejs");
 const randomstring = require("randomstring");
 
-router.route("/").get((req, res) => {
-  res.json({
-    test: "Get current user details, if any"
-  });
-});
-
-router.route("/register").post((req, res) => {
-  req.app.get("models").user.create({
-    username: req.body.username,
-    password: bcrypt.hashSync(req.body.password)
-  }).then(user => {
-    res.json({
-      ok: true
+router
+  .post("/register", (req, res) => {
+    req.app.get("models").user.create({
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password)
+    }).then(user => {
+      res.json({
+        ok: true
+      });
+    }).catch(err => {
+      res.json({
+        ok: false,
+        message: err.message || err
+      });
     });
-  }).catch(err => {
-    res.json({
-      ok: false,
-      message: err.message || err
-    });
-  });
-});
+  })
+  .post("/login", (req, res) => {
+    req.app.get("models").user.find({
+      where: {
+        username: req.body.username
+      }
+    }).then(user => {
+      if (!bcrypt.compareSync(req.body.password, user.password)) {
+        throw new Error("Username or password is incorrect");
+      }
 
-router.route("/login").post((req, res) => {
-  req.app.get("models").user.find({
-    where: {
-      username: req.body.username
-    }
-  }).then(user => {
-    if (!bcrypt.compareSync(req.body.password, user.password)) {
-      throw new Error("Username or password is incorrect");
+      const expiry = new Date();
+      expiry.setHours(expiry.getHours() + 1);
+
+      return req.app.get("models").token.create({
+        userId: user.id,
+        token: randomstring.generate(60),
+        expiresAt: expiry
+      })
+    }).then(token => {
+      return res.json({
+        ok: true,
+        token: token.token,
+        expires: token.expiresAt
+      })
+    }).catch(err => {
+      return res.json({
+        ok: false,
+        message: err.message || err
+      });
+    });
+  })
+  .post("/refresh", (req, res) => {
+    if (!req.authenticated) {
+      return res.json({
+        ok: false,
+        message: "You must be authenticated"
+      });
     }
 
     const expiry = new Date();
     expiry.setHours(expiry.getHours() + 1);
 
-    return req.app.get("models").token.create({
-      userId: user.id,
-      token: randomstring.generate(60),
+    req.app.get("models").token.update({
       expiresAt: expiry
-    })
-  }).then(token => {
-    return res.json({
-      ok: true,
-      token: token.token,
-      expires: token.expiresAt
-    })
-  }).catch(err => {
-    return res.json({
-      ok: false,
-      message: err.message || err
-    });
-  });
-});
-
-router.route("/refresh").post((req, res) => {
-  if (!req.authenticated) {
-    return res.json({
-      ok: false,
-      message: "You must be authenticated"
-    });
-  }
-
-  const expiry = new Date();
-  expiry.setHours(expiry.getHours() + 1);
-
-  req.app.get("models").token.update({
-    expiresAt: expiry
-  }, {
-    where: {
-      id: req.token.id
-    }
-  }).then(records => {
-    return res.json({
-      ok: true,
-      expires: expiry
-    });
-  }).catch(err => {
-    return res.json({
-      ok: false,
-      message: err.message || err
-    });
-  });
-});
-
-router.route("/logout").post((req, res) => {
-  if (req.authenticated) {
-    return req.app.get("models").token.destroy({
+    }, {
       where: {
         id: req.token.id
       }
+    }).then(records => {
+      return res.json({
+        ok: true,
+        expires: expiry
+      });
+    }).catch(err => {
+      return res.json({
+        ok: false,
+        message: err.message || err
+      });
     });
-  }
+  })
+  post("/logout", (req, res) => {
+    if (req.authenticated) {
+      return req.app.get("models").token.destroy({
+        where: {
+          id: req.token.id
+        }
+      });
+    }
 
-  return res.json({
-    ok: true
-  });
-});
+    return res.json({
+      ok: true
+    });
+  })
+;
 
 module.exports = router;
 
