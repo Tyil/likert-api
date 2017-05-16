d4689e539df9fdf8fd18e58ccf6e44ccd41b8d51
Patrick Spek <p.spek@tyil.nl>
2017-05-16 14:11:55 +0200

    Reindent bearer middleware

--- src/middlewares/bearer.js
+++ src/middlewares/bearer.js
@@ -1,41 +1,40 @@
 module.exports = (req, res ,next) => {
-  if (!req.headers.auth) {
-	req.authenticated = false;
+	if (!req.headers.auth) {
+		req.authenticated = false;
 
-	return next();
-  }
-
-  const megumin = req.headers.auth.split(" ");
-
-  if (megumin[0] != "Bearer") {
-	return next();
-  }
-
-  req.app.get("models").token.find({
-	where: {
-	  token: megumin[1]
-	}
-  }).then(token => {
-	if (token == null) {
-	  throw new Error("Given Bearer token is not accepted");
+		return next();
 	}
 
-	const now = new Date().getTime();
-	const expiry = new Date(token.expiresAt).getTime();
+	const megumin = req.headers.auth.split(" ");
 
-	if (expiry < now) {
-	  throw new Error("Bearer token expired");
+	if (megumin[0] != "Bearer") {
+		return next();
 	}
 
-	req.authenticated = true;
-	req.token = token;
+	req.app.get("models").token.find({
+		where: {
+			token: megumin[1]
+		}
+	}).then(token => {
+		if (token === null) {
+			throw new Error("Given Bearer token is not accepted");
+		}
 
-	return next();
-  }).catch(err => {
-	console.error(err);
-	req.authenticated = false;
+		const now = new Date().getTime();
+		const expiry = new Date(token.expiresAt).getTime();
 
-	return next();
-  });
+		if (expiry < now) {
+			throw new Error("Bearer token expired");
+		}
+
+		req.authenticated = true;
+		req.token = token;
+
+		return next();
+	}).catch(err => {
+		console.error(err);
+		req.authenticated = false;
+
+		return next();
+	});
 };
-
