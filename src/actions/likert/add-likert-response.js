3defbc3cecb4f2fa1c3e3d466edbcd6294e02936
Jeroen Vijgen <JJVijgen@gmail.com>
2017-05-20 22:52:41 +0200

    Added likert responses
    :X

--- src/actions/likert/add-likert-response.js
+++ src/actions/likert/add-likert-response.js
@@ -1,12 +1,28 @@
 const likert = require("../../models").likert_template,
 	likert_values = require("../../models").likert_template_result;
-module.exports = (likertTemplateId, userId, songId, moodId, scaleId, scaleScore) => {
-
+module.exports = (likertTemplateId, userId, songId, moodId, scaleScore) => {
 	return likert.findOne({
 		where: {
 			id: likertTemplateId
 		}
 	}).then(result => {
-
+		var response = {
+			ok: false,
+			message: 'The likert template could not be found.'
+		};
+		if (result === null) {
+			return response;
+		}
+		return likert.create({
+			userId: userId,
+			templateId: likertTemplateId,
+			songId: songId,
+			moodId: moodId,
+			scaleScore: scaleScore
+		}).then(result => {
+			response.ok = true;
+			response.message = 'The score has been added.';
+			return response;
+		});
 	});
 };
