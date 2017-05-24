cdc659cf59b346fece2cda52a2f7d00bf0e139c9
Patrick Spek <p.spek@tyil.nl>
2017-05-24 07:51:21 +0200

    Remove unused model import in the artists controller

--- src/controller/artists.js
+++ src/controller/artists.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const Artist = require("../models").artist;
 const unauthenticatedError = require("../responses/unauthenticated.json");
 
 const actions = {
