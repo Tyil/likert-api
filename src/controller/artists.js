85b8d3038a00f7191122085cfbf41004b4abcf63
Patrick Spek <p.spek@tyil.nl>
2017-05-24 07:46:34 +0200

    Update artists controller to use actions

--- src/controller/artists.js
+++ src/controller/artists.js
@@ -2,60 +2,32 @@ const router = require("express").Router();
 const Artist = require("../models").artist;
 const unauthenticatedError = require("../responses/unauthenticated.json");
 
+const actions = {
+	create: require("../actions/artists/create"),
+	delete: require("../actions/artists/delete"),
+	findAll: require("../actions/artists/find-all"),
+	findOne: require("../actions/artists/find-one"),
+	update: require("../actions/artists/update"),
+};
+
 module.exports = router
 	.get("/", (req, res) => {
-		Artist.findAll().then(artists => {
-			return res.json(artists);
+		actions.findAll().then(response => {
+			res.json(response);
 		});
 	})
 	.get("/:id", (req, res) => {
-		return Artist.findOne({
-			where: {
-				id: req.body.id
-			}
-		}).then(result => {
-			if (result === null) {
-				return res.json({
-					ok: false,
-					message: "This artist does not exist."
-				});
-			}
-
-			return res.json({
-				ok: true,
-				artist: {
-					id: result.get("id"),
-					name: result.get("name")
-				}
-			});
+		actions.findOne(req.body.id).then(response => {
+			res.json(response);
 		});
 	})
 	.post("/add", (req, res) => {
-		Artist.findOne({
-			where: {
-				name: req.body.name,
-			}
-		}).then(result => {
-			if (!req.authenticated && process.env.NODE_ENV != 'test') {
-				return res.json(unauthenticatedError);
-			}
+		if (!req.authenticated && process.env.NODE_ENV != 'test') {
+			return res.json(unauthenticatedError);
+		}
 
-			if (result !== null) {
-				return res.json({
-					ok: false,
-					message: "This artist already exists."
-				});
-			}
-
-			Artist.create({
-				name: req.body.name,
-			}).then(result => {
-				return res.json({
-					ok: true,
-					message: "This artist has been added.",
-					id: result.get("id")
-				});
-			});
+		actions.create(req.body.name).then(response => {
+			res.json(response);
 		});
 	})
 	.delete("/:id", (req, res) => {
@@ -63,25 +35,8 @@ module.exports = router
 			return res.json(unauthenticatedError);
 		}
 
-		return Artist.findOne({
-			where: {
-				name: req.body.id
-			}
-		})
-		.then(result => {
-			if (result === null) {
-				return res.json({
-					ok: false,
-					message: "This artist does not exist."
-				});
-			}
-
-			result.destroy();
-
-			return res.json({
-				ok: true,
-				message: "This artist has been removed."
-			});
+		actions.delete(req.body.id).then(response => {
+			res.json(response);
 		});
 	})
 	.put("/:id", (req, res) => {
@@ -89,31 +44,8 @@ module.exports = router
 			return res.json(unauthenticatedError);
 		}
 
-		return Artist.findOne({
-			where: {
-				id: req.body.id
-			}
-		}).then(result => {
-			if (result === null) {
-				return res.json({
-					ok: false,
-					message: "This artist does not exist."
-				});
-			}
-
-			return result.update({
-				name: req.body.name
-			}).then(result => {
-				return res.json({
-					ok: true,
-					message: "This artist has been updated."
-				});
-			}).catch(err => {
-				return res.json({
-					ok: false,
-					message: err.message || err
-				});
-			});
+		actions.update(req.body.id, req.body.name).then(response => {
+			res.json(response);
 		});
 	})
 	;
