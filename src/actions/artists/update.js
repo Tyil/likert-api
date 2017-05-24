4e3713cb033024ca49d5d882ea00383847630aa8
Patrick Spek <p.spek@tyil.nl>
2017-05-24 07:46:20 +0200

    Add update action for artists

---
+++ src/actions/artists/update.js
@@ -0,0 +1,30 @@
+const Artist = require("../models").artist;
+
+module.exports = (id, name) => {
+	return Artist.findOne({
+		where: {
+			id: id
+		}
+	}).then(result => {
+		if (result === null) {
+			return {
+				ok: false,
+				message: "This artist does not exist."
+			};
+		}
+
+		return result.update({
+			name: name
+		}).then(result => {
+			return {
+				ok: true,
+				message: "This artist has been updated."
+			};
+		}).catch(err => {
+			return {
+				ok: false,
+				message: err.message || err
+			};
+		});
+	});
+};
