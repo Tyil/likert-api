ea5b288818e1b28e811b020435cc2ebb86fbd7aa
Patrick Spek <p.spek@tyil.nl>
2017-05-24 07:45:49 +0200

    Add delete action for artists

---
+++ src/actions/artists/delete.js
@@ -0,0 +1,24 @@
+const Artist = require("../models").artist;
+
+module.exports = id => {
+	return Artist.findOne({
+		where: {
+			id: id
+		}
+	})
+	.then(result => {
+		if (result === null) {
+			return {
+				ok: false,
+				message: "This artist does not exist."
+			};
+		}
+
+		result.destroy();
+
+		return {
+			ok: true,
+			message: "This artist has been removed."
+		};
+	});
+};
