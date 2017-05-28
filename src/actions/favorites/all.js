1cd56e84f382c8eb50bf18644f01dcf46a672a76
Jeroen Vijgen <JJVijgen@gmail.com>
2017-05-28 19:24:15 +0200

    Get all preferences back

--- src/actions/favorites/all.js
+++ src/actions/favorites/all.js
@@ -1,8 +1,52 @@
-const genre = require("../../models").genre,
-	artist = require("../../models").artist,
-	album = require("../../models").album,
-	song = require("../../models").song;
+const user = require("../../models").user,
+	favo_album = require("../../models").favorite_album,
+	favo_artist = require("../../models").favorite_artist,
+	favo_genre = require("../../models").favorite_genre,
+	favo_song = require("../../models").favorite_song;
 
 module.exports = (userId) => {
-
+	var response = {
+		ok: false,
+		message: ''
+	}
+	return user.findOne({
+		where: {
+			id: userId
+		}
+	}).then(result => {
+		if (result === null) {
+			response.message = 'The requested information could not be found';
+			return response;
+		}
+		reponse.ok = true;
+		favo_album.findAll({
+			where: {
+				userId: userId
+			}
+		}).then(result => {
+			response.message += result;
+		});
+		favo_artist.findAll({
+			where: {
+				userId: userId
+			}
+		}).then(result => {
+			response.message += result;
+		});
+		favo_genre.findAll({
+			where: {
+				userId: userId
+			}
+		}).then(result => {
+			response.message += result;
+		});
+		favo_song.findAll({
+			where: {
+				userId: userId
+			}
+		}).then(result => {
+			response.message += result;
+		});
+		return response;
+	});
 };
