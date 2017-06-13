a0986c9e4e6431f146c47808051fb7f87cf86d76
Patrick Spek <p.spek@tyil.nl>
2017-06-13 19:10:03 +0200

    Add albums to seeders

---
+++ src/seeders/20170520111749-albums.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const entries = [
+	{
+		id: 1,
+		artistId: 1,
+		name: "A Night at the Opera"
+	},
+	{
+		id: 2,
+		artistId: 2,
+		name: "Oceans - The String Quartet Tribute to Enya"
+	},
+	{
+		id: 3,
+		artistId: 3,
+		name: "Shepherd Moons"
+	},
+	{
+		id: 4,
+		artistId: 3,
+		name: "Watermark"
+	},
+	{
+		id: 5,
+		artistId: 4,
+		name: "'V' is for Vagina"
+	},
+	{
+		id: 6,
+		artistId: 5,
+		name: "Heroes are Hard to Find"
+	},
+	{
+		id: 7,
+		artistId: 6,
+		name: "Makara"
+	},
+	{
+		id: 8,
+		artistId: 7,
+		name: "Kaya"
+	},
+	{
+		id: 9,
+		artistId: 8,
+		name: "7 Seben"
+	},
+	{
+		id: 10,
+		artistId: 9,
+		name: "LOL (^^,)"
+	},
+	{
+		id: 11,
+		artistId: 10,
+		name: "Caravan Palace"
+	}
+];
+
+module.exports = {
+	up: function (queryInterface, Sequelize) {
+		return queryInterface.bulkInsert("artusts", entries);
+	},
+
+	down: function (queryInterface, Sequelize) {
+		return queryInterface.bulkDelete("artists", {
+			id: entries.map(x => x.id)
+		});
+	}
+};
