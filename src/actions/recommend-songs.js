4a81616d021e9f83f4840301144428c054c5db07
Ramko <rhrbooga@avans.nl>
2017-06-14 20:57:39 +0200

    Almost newMusic
    Grootste lijn voor nog niet beluisterde muziek (nog even met jeroen
    bekijken)

--- src/actions/recommend-songs.js
+++ src/actions/recommend-songs.js
@@ -1,27 +1,56 @@
 const song = require("../models").song,
-	genre = require("../models").genre;
+	genre = require("../models").genre,
+	user = require("../models").user,
+	likeGenre = require("../models").like_dislike_genre;
+
+	var songs,temp = []
 
 module.exports = (genreId) => {
-	return genre.findOne({
+	// return genre.findOne({
+	// 	where: {
+	// 		GenreId: genreId
+	// 	}
+	// }).then(result => {
+	// 	if(result === null){
+	// 		return {
+	// 			ok: false,
+	// 			message: 'Genre could not be found.'
+	// 		};
+	// 	}
+	// 	return song.findAll({
+	// 		where: {
+	// 			Mood_MoodId: result.genreId
+	// 		}
+	// 	}).then(result => {
+	// 		return {
+	// 			ok: true,
+	// 			message: result
+	// 		}
+	// 	});
+	// });
+};
+
+function newMusic(genreId, moodId){ //, artistId, albumId){
+	song.findall({
 		where: {
-			GenreId: genreId
+			Mood_MoodId: result.genreId & result.moodId
 		}
 	}).then(result => {
-		if(result === null){
-			return {
-				ok: false,
-				message: 'Genre could not be found.'
-			};
+		for(var i = 0; result.length > i; i++){
+			//songResult.findOne({
+				//where: {
+				//	SongId: songId
+				//}
+			//}).then(result => {
+			// 	if (result.length == 0){
+			// 		temp.push(result.songId);
+			// 	}
+			//});
 		}
-		return song.findAll({
-			where: {
-				Mood_MoodId: result.genreId
-			}
-		}).then(result => {
-			return {
-				ok: true,
-				message: result
-			}
-		});
+		var j = 0;
+		for(var i = 10; songs.length < i; i--){
+			songs[i] = {songId: temp[j], listened: "no"};
+			j++;
+		}
 	});
-};
+}
