5c1c8d1d5591964dd6197db0174280b556989c2b
Ramko <rhrbooga@avans.nl>
2017-06-15 10:54:45 +0200

    migrate script

--- src/migrations/20170615084350-create-previously-listened.js
+++ src/migrations/20170615084350-create-previously-listened.js
@@ -2,22 +2,26 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable('previously_listened', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      songId: {
+        allowNull: false,
+        references: {
+          model: 'songs',
+          key: 'id'
+        },
+        type: DataTypes.INTEGER
+      }
+    }
+    )
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('previously_listened');
   }
 };
