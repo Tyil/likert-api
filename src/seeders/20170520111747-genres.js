8e4743e5acd9b64d7f523e2172cab40cf43e9897
Patrick Spek <p.spek@tyil.nl>
2017-06-16 16:57:42 +0200

    Add new songs, albums and genres to our seeds

--- src/seeders/20170520111747-genres.js
+++ src/seeders/20170520111747-genres.js
@@ -3,22 +3,13 @@
 module.exports = {
 	up: function (queryInterface, Sequelize) {
 		return queryInterface.bulkInsert("genres", [
-			{
-				id: 1,
-				name: "Rock",
-			},
-			{
-				id: 2,
-				name: "Soul",
-			},
-			{
-				id: 3,
-				name: "Jazz",
-			},
-			{
-				id: 4,
-				name: "Ambient",
-			}
+			{ id: 1, name: "Rock", },
+			{ id: 2, name: "Soul", },
+			{ id: 3, name: "Jazz", },
+			{ id: 4, name: "Ambient", },
+			{ id: 5, name: "New Age", },
+			{ id: 6, name: "Orchestral", },
+			{ id: 7, name: "Classical", },
 		]);
 	},
 
--- src/seeders/20170520111749-albums.js
+++ src/seeders/20170520111749-albums.js
@@ -1,61 +1,17 @@
 'use strict';
 
 const entries = [
-	{
-		id: 1,
-		artistId: 1,
-		name: "A Night at the Opera"
-	},
-	{
-		id: 2,
-		artistId: 2,
-		name: "Oceans - The String Quartet Tribute to Enya"
-	},
-	{
-		id: 3,
-		artistId: 3,
-		name: "Shepherd Moons"
-	},
-	{
-		id: 4,
-		artistId: 3,
-		name: "Watermark"
-	},
-	{
-		id: 5,
-		artistId: 4,
-		name: "'V' is for Vagina"
-	},
-	{
-		id: 6,
-		artistId: 5,
-		name: "Heroes are Hard to Find"
-	},
-	{
-		id: 7,
-		artistId: 6,
-		name: "Makara"
-	},
-	{
-		id: 8,
-		artistId: 7,
-		name: "Kaya"
-	},
-	{
-		id: 9,
-		artistId: 8,
-		name: "7 Seben"
-	},
-	{
-		id: 10,
-		artistId: 9,
-		name: "LOL (^^,)"
-	},
-	{
-		id: 11,
-		artistId: 10,
-		name: "Caravan Palace"
-	}
+	{ id: 1, artistId: 1, name: "A Night at the Opera", },
+	{ id: 2, artistId: 2, name: "Oceans - The String Quartet Tribute to Enya", },
+	{ id: 3, artistId: 3, name: "Shepherd Moons", },
+	{ id: 4, artistId: 3, name: "Watermark", },
+	{ id: 5, artistId: 4, name: "'V' is for Vagina", },
+	{ id: 6, artistId: 5, name: "Heroes are Hard to Find", },
+	{ id: 7, artistId: 6, name: "Makara", },
+	{ id: 8, artistId: 7, name: "Kaya", },
+	{ id: 9, artistId: 8, name: "7 Seben", },
+	{ id: 10, artistId: 9, name: "LOL (^^,)", },
+	{ id: 11, artistId: 10, name: "Caravan Palace", }
 ];
 
 module.exports = {
