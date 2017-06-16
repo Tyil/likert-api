b9dabed92311b4714a44b5dbf9fb070d59137065
Patrick Spek <p.spek@tyil.nl>
2017-06-16 17:07:17 +0200

    Add missing commas

--- src/seeders/20170520110852-users.js
+++ src/seeders/20170520110852-users.js
@@ -3,8 +3,8 @@
 module.exports = {
 	up: function (queryInterface, Sequelize) {
 		return queryInterface.bulkInsert("users", [
-			{ username: "admin", password: "$2y$10$e0tQXVE4a.ozEFM90yg0puhYy7z/dJLiWdwtG6ljelSV8P1RWTQrm", createdAt: "1970-01-01 00:00:00", updatedAt: "1970-01-01 00:00:00", }
-			{ username: "tyil", password: "$2y$10$RDEYRNzevPxSPWQbXttk4ut.QbcaSE5Zh3JOeQbonvkNOFWNZd2Bi", createdAt: "1970-01-01 00:00:00", updatedAt: "1970-01-01 00:00:00", }
+			{ username: "admin", password: "$2y$10$e0tQXVE4a.ozEFM90yg0puhYy7z/dJLiWdwtG6ljelSV8P1RWTQrm", createdAt: "1970-01-01 00:00:00", updatedAt: "1970-01-01 00:00:00", },
+			{ username: "tyil", password: "$2y$10$RDEYRNzevPxSPWQbXttk4ut.QbcaSE5Zh3JOeQbonvkNOFWNZd2Bi", createdAt: "1970-01-01 00:00:00", updatedAt: "1970-01-01 00:00:00", },
 		]);
 	},
 
