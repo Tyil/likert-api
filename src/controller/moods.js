1a79aca9b61bb1fdfc4630b2ebb1376e4fc61ab8
Jeroen Vijgen <JJVijgen@gmail.com>
2017-06-17 23:15:41 +0200

    Added personal mood

--- src/controller/moods.js
+++ src/controller/moods.js
@@ -3,7 +3,9 @@ const router = require("express").Router(),
 	remove = require('../actions/moods/remove-moods'),
 	update = require('../actions/moods/update-moods'),
 	specific = require('../actions/moods/specific-moods'),
+	update_user_mood = require('../actions/moods/update-user-mood'),
 	moods = require('../models').mood,
+	current = require('../models').current_mood,
 	notLoggedIn = require("../responses/unauthenticated.json");
 
 module.exports = router
@@ -13,6 +15,26 @@ module.exports = router
 				ok: true,
 				message: result
 			});
+		});
+	})
+	.get('/my', (req, res) => {
+		if (!req.authenticated) {
+			return res.json(notLoggedIn);
+		}
+		current.findOne({
+			where: {
+				userId: req.token.userId
+			}
+		}).then(result => {
+			return res.json(result);
+		});
+	})
+	.post('/my', (req, res) => {
+		if (!req.authenticated) {
+			return res.json(notLoggedIn);
+		}
+		update_user_mood(req.body.moodId, req.token.userId).then(result => {
+			return res.json(result);
 		});
 	})
 	.get('/:mood', (req, res) => {
