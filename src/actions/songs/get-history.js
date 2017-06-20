daf406e6b8c7d63608bbe2fba17cf4dfeac65387
Patrick Spek <p.spek@tyil.nl>
2017-06-20 23:19:13 +0200

    Add action to retrieve recent songs

---
+++ src/actions/songs/get-history.js
@@ -0,0 +1,25 @@
+const history = require("../../models").history_song;
+
+module.export = (userId, count) => {
+	return history.findAll({
+		where: {
+			userId: userId
+		},
+		limit: count,
+		order: [
+			"createdAt",
+			"DESC"
+		]
+	}).then(results => {
+		return {
+			ok: true,
+			message: results
+		};
+	}).catch(err => {
+		return {
+			ok: false,
+			message: err.message || err
+		};
+	});
+};
+
