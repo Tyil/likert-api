a52446a4c5529f96e9826d14b715e77be2a66456
Patrick Spek <p.spek@tyil.nl>
2017-06-21 17:58:16 +0200

    Update bcrypt identifier so nodejs' bcrypt will play nice

--- src/seeders/20170520110852-users.js
+++ src/seeders/20170520110852-users.js
@@ -3,8 +3,8 @@
 module.exports = {
 	up: function (queryInterface, Sequelize) {
 		return queryInterface.bulkInsert("users", [
-			{ username: "admin", password: "$2y$10$/KJgA47a/kA/vKvrcPIc1ushSR//HdUrdSPskwVf30/0U0Ro6WwxS", createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() },
-			{ username: "tyil", password: "$2y$10$RDEYRNzevPxSPWQbXttk4ut.QbcaSE5Zh3JOeQbonvkNOFWNZd2Bi", createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() },
+			{ username: "admin", password: "$2a$10$/KJgA47a/kA/vKvrcPIc1ushSR//HdUrdSPskwVf30/0U0Ro6WwxS", createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() },
+			{ username: "tyil", password: "$2a$10$RDEYRNzevPxSPWQbXttk4ut.QbcaSE5Zh3JOeQbonvkNOFWNZd2Bi", createdAt: new Date().toISOString(), updatedAt: new Date().toISOString() },
 		]);
 	},
 
